e30b2860cbe148323f5da8f54e7efc14842d9442
jichuta <ichuta.josoe@gmail.com>
2025-01-16 10:20:21 -0400

    adding the useCounter hook to change the url in tehe api, and add new buttons to make the request again

--- 05-hook-app/src/03-examples/MultipleCustomHooks.jsx
+++ 05-hook-app/src/03-examples/MultipleCustomHooks.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import useFetch from '../hooks/useFetch'
+import useCounter from '../hooks/useCounter';
 
 const MultipleCustomHooks = () => {
 
-    const { data, hasError, isLoading } = useFetch('https://pokeapi.co/api/v2/pokemon/3');
+    const { counter, incrementar, decrementar } = useCounter(1);
+    // console.log(JSON.stringify(counter));
+    const { data, hasError, isLoading } = useFetch(`https://pokeapi.co/api/v2/pokemon/${counter}`);
 
   return (
     <>
@@ -15,6 +18,16 @@ const MultipleCustomHooks = () => {
         <h3>{ data?.name }</h3>
 
         {/* <pre>{ JSON.stringify(data, null, 2)}</pre> */}
+
+        <button
+            className='btn btn-primary mt-2'
+            onClick={ () => counter > 1 ? decrementar() : null }
+        >Anterior</button>
+
+        <button
+            className='btn btn-primary mt-2'
+            onClick={ () => incrementar() }
+        >Siguiente</button>
     </>
   )
 }
