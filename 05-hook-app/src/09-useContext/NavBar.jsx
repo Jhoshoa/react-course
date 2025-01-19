176af41afa39ab73787b1fb6ea0239d124e5efad
jichuta <ichuta.josoe@gmail.com>
2025-01-19 16:20:21 -0400

    update the navbar component using the NavLink and isActive argument

--- 05-hook-app/src/09-useContext/NavBar.jsx
+++ 05-hook-app/src/09-useContext/NavBar.jsx
@@ -1,14 +1,48 @@
 import React from 'react'
-import { Link } from 'react-router'
+import { Link, NavLink } from 'react-router'
 
 const NavBar = () => {
-  return (
-    <>
-        <Link to='/'>Home</Link>
-        <Link to='/about'>About</Link>
-        <Link to='/login'>Login</Link>
-    </>
-  )
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark rounded-3">
+                <div className="container-fluid">
+                    <Link className="navbar-brand" to="/">useContext</Link>
+
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <NavLink
+                                className={({ isActive }) => {
+                                    return `nav-link ${isActive ? 'active' : ''}`
+                                }}
+                                to='/'
+                            >
+                                Home
+                            </NavLink>
+
+                            <NavLink
+                                className={({ isActive }) => {
+                                    return `nav-link ${isActive ? 'active' : ''}`
+                                }}
+                                to='/about'
+                            >
+                                About
+                            </NavLink>
+
+                            <NavLink
+                                className={({ isActive }) => {
+                                    return `nav-link ${isActive ? 'active' : ''}`
+                                }}
+                                to='/login'
+                            >
+                                Login
+                            </NavLink>
+
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
 }
 
 export default NavBar
