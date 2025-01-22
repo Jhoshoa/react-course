9a8b11aea9c6b4bacd0d204b0f1e5bcf0778b6fa
jichuta <ichuta.josoe@gmail.com>
2025-01-22 16:18:35 -0400

    Create a second routes for a part of the application, basically to separate some component like navbar form other pages

---
+++ 07-heroes-spa/src/heroes/routes/HeroesRoutes.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { NavBar } from '../../ui'
+import { Navigate, Route, Routes } from 'react-router'
+import { DcPage, HeroPage, MarvelPage, SearchPage } from '../pages'
+
+export const HeroesRoutes = () => {
+  return (
+    <>
+        <NavBar />
+
+        <Routes>
+            <Route path='marvel' element={ <MarvelPage />} />
+            <Route path='/dc' element={ <DcPage />} />
+            <Route path='/search' element={ <SearchPage />} />
+            <Route path='/hero' element={ <HeroPage />} />
+
+            <Route path='/*' element={ <Navigate to='/marvel' />} />
+        </Routes>
+    </>
+  )
+}
--- 07-heroes-spa/src/router/AppRouter.jsx
+++ 07-heroes-spa/src/router/AppRouter.jsx
@@ -2,18 +2,15 @@ import React from 'react'
 import { Routes, Route, Navigate } from 'react-router'
 import LoginPage from '../auth/pages/LoginPage'
 import { NavBar } from '../ui'
-import { MarvelPage, DcPage } from '../heroes'
+import { MarvelPage, DcPage, HeroesRoutes } from '../heroes'
 
 export const AppRouter = () => {
   return (
     <>
-        <NavBar />
         <Routes>
-            <Route path='marvel' element={ <MarvelPage />} />
-            <Route path='/dc' element={ <DcPage />} />
             <Route path='/login' element={ <LoginPage />} />
 
-            <Route path='/*' element={ <Navigate to='/marvel' />} />
+            <Route path='/*' element={ <HeroesRoutes /> } />
         </Routes>
     </>
   )
