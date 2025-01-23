efa506b1c2ecdd1bff80832721a0a33df0871502
jichuta <ichuta.josoe@gmail.com>
2025-01-22 21:36:58 -0400

    listing the heroes after the search and add a function to get heroes by name

--- 07-heroes-spa/src/heroes/pages/SearchPage.jsx
+++ 07-heroes-spa/src/heroes/pages/SearchPage.jsx
@@ -3,6 +3,7 @@ import queryString from 'query-string'
 import { HeroCard } from '../components/HeroCard'
 import useForm from '../../hook/useForm'
 import { useLocation, useNavigate } from 'react-router'
+import { getHeroesByName } from '../helpers'
 
 export const SearchPage = () => {
 
@@ -12,8 +13,10 @@ export const SearchPage = () => {
 
   const { q = '' } = queryString.parse( location.search );
 
+  const heroes = getHeroesByName( q );
+
   const { searchText, onInputChange} = useForm({
-    searchText: ''
+    searchText: q
   });
 
   const onSearchSubmit = (event) => {
@@ -57,14 +60,18 @@ export const SearchPage = () => {
           <h4>Results</h4>
           <hr />
 
-          <div className="alert alert-primary">
-            Search a hero
-          </div>
+          {
+            ( q === '' )
+            ? <div className="alert alert-primary">Search a hero</div>
+            : ( heroes.length === 0 )
+              && <div className="alert alert-danger">No hero with <b>{ q }</b></div>
+          }
 
-          <div className="alert alert-danger">
-          No hero with <b>{ q }</b>
-          </div>
-
+          {
+            heroes.map( (hero) => (
+              <HeroCard key={hero.id} {...hero} />
+            ))
+          }
           {/* <HeroCard /> */}
 
         </div>
