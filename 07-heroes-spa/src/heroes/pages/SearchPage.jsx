db504947ba03617f485f0216b433d75d85f4160f
jichuta <ichuta.josoe@gmail.com>
2025-01-22 21:48:23 -0400

    another way of hidding and showing part of jsx with functions and styles

--- 07-heroes-spa/src/heroes/pages/SearchPage.jsx
+++ 07-heroes-spa/src/heroes/pages/SearchPage.jsx
@@ -15,6 +15,9 @@ export const SearchPage = () => {
 
   const heroes = getHeroesByName( q );
 
+  const showSearch = (q.length === 0);
+  const showError = (q.length > 0) && heroes.length === 0;
+
   const { searchText, onInputChange} = useForm({
     searchText: q
   });
@@ -22,7 +25,7 @@ export const SearchPage = () => {
   const onSearchSubmit = (event) => {
     event.preventDefault();
 
-    if ( searchText.trim().length <= 1 ) return;
+    //if ( searchText.trim().length <= 1 ) return;
 
     //console.log({ searchText });
 
@@ -60,12 +63,24 @@ export const SearchPage = () => {
           <h4>Results</h4>
           <hr />
 
-          {
+          {/* {
             ( q === '' )
             ? <div className="alert alert-primary">Search a hero</div>
             : ( heroes.length === 0 )
               && <div className="alert alert-danger">No hero with <b>{ q }</b></div>
-          }
+          } */}
+
+          <div className="alert alert-primary animate__animated animate__fadeIn"
+            style={{display: showSearch ? '' : 'none'}}
+          >
+            Search a hero
+          </div>
+
+          <div className="alert alert-danger animate__animated animate__fadeIn"
+            style={{ display: showError ? '': 'none'}}
+          >
+            No hero with <b>{ q }</b>
+          </div>
 
           {
             heroes.map( (hero) => (
